c790c389f4fc1926554de667863e78ca0b0c4bff
Velko <wlsgurrla716@naver.com>
2025-07-19 20:15:06 +0900

    🔧[Chore] - 프로젝트 초기설정

---
+++ Muje/Muje/MujeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MujeApp.swift
+//  Muje
+//
+//  Created by 김진혁 on 7/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MujeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
