207ff30d77f9ad14b4d43e87cae66ff77883782f
Velko <wlsgurrla716@naver.com>
2025-08-05 16:48:07 +0900

    🔧[Chore] - Firestore 에러 처리 추가

---
+++ Muje/Muje/Common/Enum/Error/FirestoreError.swift
@@ -0,0 +1,35 @@
+//
+//  FirestoreError.swift
+//  Muje
+//
+//  Created by 김진혁 on 8/1/25.
+//
+
+import Foundation
+
+enum Error: LocalizedError {
+    case addFailed(underlying: Swift.Error)
+    case fetchFailed(underlying: Swift.Error)
+    case deleteFailed(underlying: Swift.Error)
+    case updateFailed(underlying: Swift.Error)
+    
+    case decodingFailed
+    case encodingFailed
+    
+    var errorDescription: String? {
+        switch self {
+        case .addFailed(let error):
+            "데이터를 추가하는데 실패했습니다: \(error)"
+        case .fetchFailed(let error):
+            "데이터를 읽어오는데 실패했습니다: \(error)"
+        case .deleteFailed(let error):
+            "데이터를 삭제하는데 실패했습니다: \(error)"
+        case .updateFailed(let error):
+            "데이터를 업데이트하는데 실패했습니다: \(error)"
+        case .encodingFailed:
+            "데이터를 encoding하는데 실패했습니다"
+        case .decodingFailed:
+            "데이터를 decoding하는데 실패했습니다"
+        }
+    }
+}
