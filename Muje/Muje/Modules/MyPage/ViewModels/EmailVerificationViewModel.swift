d79d5e629f5a1a09da5c23979d90f2d7fc9f38e9
Velko <wlsgurrla716@naver.com>
2025-08-06 13:37:05 +0900

    ✨[Feat]#2 - 이메일 인증 임시 뷰 구현

---
+++ Muje/Muje/Modules/MyPage/ViewModels/EmailVerificationViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  MyPageViewModel.swift
+//  Muje
+//
+//  Created by 김진혁 on 7/20/25.
+//
+
+import Foundation
+
+@Observable
+final class EmailVerificationViewModel {
+    
+    
+    
+    
+    func sendVerificationEmail() {
+        
+    }
+}
--- Muje/Muje/Modules/MyPage/Views/EmailVerificationView .swift
+++ Muje/Muje/Modules/MyPage/Views/EmailVerificationView .swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct EmailVerificationView : View {
+    @State private var viewModel: EmailVerificationViewModel = .init()
+    @State private var emailText: String = ""
+    
     var body: some View {
-        Text("EmailVerificationView")
+        VStack {
+            Spacer()
+            TitleView
+            Spacer().frame(height: 20)
+            InputEmailTextFieldView
+            Spacer()
+            bottomButtonView
+            Spacer()
+        }
+        .paddingH16()
     }
+    
+    
+    // MARK: - 이메일 인증 타이틀 뷰
+    private var TitleView: some View {
+        VStack(alignment: .leading) {
+            Text("학교 인증을 위해서\n대학교 이메일을 입력해주세요.(1/3)")
+                .font(.largeTitle)
+                .bold()
+        }
+    }
+    
+    // MARK: - 이메일 입력 텍스트 필드 뷰
+    private var InputEmailTextFieldView: some View {
+        VStack(alignment: .leading) {
+            Text("학교 이메일")
+                .font(.caption)
+                .bold()
+            
+            HStack(spacing: .zero) {
+                TextField("이메일을 입력해주세요.", text: $emailText)
+                Text("@jbnu.ac.kr")
+                    .font(.caption)
+                    .foregroundStyle(Color.gray)
+            }
+        }
+    }
+    
+    // MARK: - 바텀 아래 인증 요청 뷰
+    private var bottomButtonView: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Text("인증 요청")
+            }
+        }
+    }
+    
+    
 }
 
 #Preview {
