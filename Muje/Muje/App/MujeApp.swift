3a52993745125f1bb6f41d4388536da74e544329
Velko <wlsgurrla716@naver.com>
2025-08-10 18:26:47 +0900

    ✨[Feat]#2 - 딥링크 설정

--- Muje/Muje/App/MujeApp.swift
+++ Muje/Muje/App/MujeApp.swift
@@ -7,33 +7,60 @@
 
 import SwiftUI
 import FirebaseCore
+import UserNotifications
+import FirebaseAuth
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in
+            
+        }
+        
+        application.registerForRemoteNotifications()
+        
+        return true
+    }
+    
+    // MARK: - 휴대전화 인증 시 필요
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        if Auth.auth().canHandleNotification(userInfo) {
+            // Firebase 인증 관련 알림이면 처리 완료로 반환
+            completionHandler(.noData)
+            return
+        }
 
-    return true
-  }
+        // 다른 알림 처리 (예: FCM 등)
+        completionHandler(.noData)
+    }
 }
 
 
 @main
 struct MujeApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    @State private var signUpContainer = SignUpContainer()
     @StateObject var router: NavigationRouter = .init()
     
     var body: some Scene {
         WindowGroup {
-            RootView()
+            RootView() // FIXME: - RootView로 변경하기
                 .onOpenURL { url in
                         Task {
                             do {
-                                let isNewUser = try await FirebaseAuthManager.shared.handleEmailSignInLink(url: url, inputEmail: "rlawlsgur7@postech.ac.kr") // FIXME: - 이메일 수정
+                                let isNewUser = try await FirebaseAuthManager.shared.handleEmailSignInLink(
+                                    url: url,
+                                    inputEmail: FirebaseAuthManager.shared.email
+                                )
                                 
                                 if isNewUser {
                                     router.push(to: .userInfoInputView) // 회원 가입 뷰로 이동
                                 } else {
+                                    FirebaseAuthManager.shared.email = "" // 이메일 초기화
                                     // TODO: 로그인 완료, 키체인 유저 정보 입력, 마이 페이지 루트 뷰로 이동
                                 }
                             } catch {
@@ -41,7 +68,7 @@ struct MujeApp: App {
                             }
                         }
                     }
-            
+                .environment(signUpContainer)
                 .environmentObject(router)
         }
     }
