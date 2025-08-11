b8e0d6a520ec395f31a0105c1c485b536265191c
jHoon99 <eldkf1597@naver.com>
2025-08-12 02:25:35 +0900

    ✨[Feat]#5 - 비정규화 객체 모델 구현
    지원자 관리화면에서 Header의 post정보

---
+++ Muje/Muje/Models/ViewData/ApplicationManagementPostInfo.swift
@@ -0,0 +1,25 @@
+//
+//  ApplicationManagementPostInfo.swift
+//  Muje
+//
+//  Created by 조재훈 on 8/11/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct ApplicationManagementPostInfo: Equatable, Hashable {
+  let title: String
+  let organization: String
+  let recruitmentStart: Timestamp
+  let recruitmentEnd: Timestamp
+  let status: String
+  
+  init(from post: Post) {
+    self.title = post.title
+    self.organization = post.organization
+    self.recruitmentStart = post.recruitmentStart
+    self.recruitmentEnd = post.recruitmentEnd
+    self.status = post.status
+  }
+}
