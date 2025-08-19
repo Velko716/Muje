35f8f611601cd99a8a7e48009064d7187fbc6d5c
Velko <wlsgurrla716@naver.com>
2025-08-19 21:39:37 +0900

    ✨[Feat]#7 - 쪽지 뷰 오른쪽 스와이프 뒤로가기 구현

--- Muje/Muje/Resource/Extension/View+.swift
+++ Muje/Muje/Resource/Extension/View+.swift
@@ -31,5 +31,26 @@ extension View {
     func paddingH16() -> some View {
         self.padding(.horizontal, 16)
     }
+    
+    /// 뷰의 오른쪽 스와이프(->)를 인식하는 메서드입니다.
+    func rightSwipeToPop(_ pop: @escaping () -> Void) -> some View {
+        overlay(
+            GeometryReader { _ in
+                Color.clear
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .contentShape(Rectangle()) // 전체 영역 터치 가능
+                    .highPriorityGesture(
+                        DragGesture(minimumDistance: 15)
+                            .onEnded { value in
+                                let dx = value.translation.width
+                                let dy = value.translation.height
+                                if dx > 80 && abs(dx) > abs(dy) {
+                                    pop()
+                                }
+                            }
+                    )
+            }
+        )
+    }
 }
 
