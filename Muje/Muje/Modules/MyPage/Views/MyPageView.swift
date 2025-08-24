38975fdd48fbc397b83d173865fef86a988007b4
Velko <wlsgurrla716@naver.com>
2025-08-24 21:14:56 +0900

    ✨[Feat]#17 - 설정 UI Toolbar 구현

--- Muje/Muje/Modules/MyPage/Views/MyPageView.swift
+++ Muje/Muje/Modules/MyPage/Views/MyPageView.swift
@@ -11,31 +11,39 @@ struct MyPageView: View {
     @EnvironmentObject private var router: NavigationRouter
     
     var body: some View {
-        VStack {
-            Button {
-                router.push(to: .emailVerificationView)
-            } label: {
-                Text("로그인 해주세요 >")
-                    .font(.largeTitle)
-                    .bold()
-            }
-            
-            Spacer().frame(height: 100)
-            
-            // FIXME: - 임시
-            Button {
-                Task {
-                    try await FirebaseAuthManager.shared.currentUserSignOut()
-                    FirebaseAuthManager.shared.currentUser = nil
+        ZStack {
+            VStack {
+                Button {
+                    router.push(to: .emailVerificationView)
+                } label: {
+                    Text("로그인 해주세요 >")
+                        .font(.largeTitle)
+                        .bold()
                 }
-            } label: {
-                Text("로그아웃")
+                
+                Spacer().frame(height: 100)
+                
+                // FIXME: - 임시
+                Button {
+                    Task {
+                        try await FirebaseAuthManager.shared.currentUserSignOut()
+                        FirebaseAuthManager.shared.currentUser = nil
+                    }
+                } label: {
+                    Text("로그아웃")
+                }
+            }
+            .toolbar {
+                ToolbarLeadingBackButton()
+                ToolbarCenterTitle(text: "설정")
             }
         }
     }
 }
 
 #Preview {
-    MyPageView()
-        .environmentObject(NavigationRouter())
+    NavigationStack {
+        MyPageView()
+            .environmentObject(NavigationRouter())
+    }
 }
