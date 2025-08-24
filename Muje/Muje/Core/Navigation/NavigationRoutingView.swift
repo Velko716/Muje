b42ac65bfc64fbb365802149bb2fdb373fee7c4f
Velko <wlsgurrla716@naver.com>
2025-08-24 20:34:43 +0900

    💄[Design]#17 - 루트 UI 탭 바 아이템 수정
    기존 탭 바에 있던 마이 페이지뷰가 홈 화면 툴 바 오른쪽 설정 버튼으로 이동

--- Muje/Muje/Core/Navigation/NavigationRoutingView.swift
+++ Muje/Muje/Core/Navigation/NavigationRoutingView.swift
@@ -54,6 +54,8 @@ struct NavigationRoutingView: View {
                 UserInfoInputView(uuid: uuid, email: email)
             case .inboxView(let conversationId):
                 InboxView(conversationId: conversationId)   
+            case .myPageView:
+                MyPageView()
             }
         }
         .hideBackButton()
--- Muje/Muje/Modules/Home/Views/TopNavigationView.swift
+++ Muje/Muje/Modules/Home/Views/TopNavigationView.swift
@@ -35,7 +35,7 @@ struct TopNavigationView: View {
             Spacer().frame(width: 16)
             //MARK: 설정 아이콘
             Button(action: {
-                router.push(to: .notificationView) //FIXME: 설정뷰로 수정
+                router.push(to: .myPageView) //FIXME: 설정뷰로 수정
             }) {
                 Image(systemName: "gearshape")
                     .font(.system(size: 26))
