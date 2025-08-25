e31cc554295851c3043d876d99c01c03c5b158c9
Velko <wlsgurrla716@naver.com>
2025-08-25 17:51:58 +0900

    💄[Design]#17 - 리스트 로그인 버튼 수정
    스크롤 고정에서, 리스트에 포함 시켜 같이 스크롤

--- Muje/Muje/Modules/MyPage/Views/MyPageView.swift
+++ Muje/Muje/Modules/MyPage/Views/MyPageView.swift
@@ -13,6 +13,9 @@ struct MyPageView: View {
     private var sections: [MyPageSection] {
         let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "-"
         return [
+            .init(header: "", rows: [
+                .init(kind: .action(title: "알림 설정", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
+            ]),
             .init(header: "커뮤니티", rows: [
                 .init(kind: .action(title: "신고 내역", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
                 .init(kind: .action(title: "차단 내역", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
@@ -20,7 +23,7 @@ struct MyPageView: View {
             ]),
             .init(header: "이용 안내", rows: [
                 .init(kind: .value(title: "앱 버전", value: appVersion)), // FIXME: - 라우터 변경
-                .init(kind: .action(title: "문의하기", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
+                .init(kind: .action(title: "문의하기", action: { print("문의하기"); router.push(to: .contentView )})), // FIXME: - 라우터 변경
                 .init(kind: .action(title: "서비스 이용약관", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
                 .init(kind: .action(title: "개인정보 처리 방침", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
                 .init(kind: .action(title: "청소년 보호 정책", action: { router.push(to: .contentView )})), // FIXME: - 라우터 변경
@@ -37,11 +40,12 @@ struct MyPageView: View {
     var body: some View {
         ZStack {
             Color.white
-            
             VStack(alignment: .leading) {
-                topUserInfoView
-                    .paddingH16()
                 List {
+                    topUserInfoView
+                        .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        .listRowBackground(Color.clear)
+                    
                     ForEach(sections) { section in
                         Section {
                             ForEach(section.rows) { row in
@@ -50,6 +54,7 @@ struct MyPageView: View {
                             .listRowBackground(Color(.secondarySystemBackground)) // FIXME: - Gray 50으로 색상 변경
                         } header: {
                             headerView(section.header)
+                                .listRowInsets(.init(top: 40, leading: 0, bottom: 8, trailing: 0)) // 리스트 Row랑 자동 정렬 맞추기
                         }
                     }
                 }
@@ -62,7 +67,6 @@ struct MyPageView: View {
                 ToolbarCenterTitle(text: "설정")
             }
         }
-        
     }
     
     // MARK: - 탑) 유저 정보 입력 뷰
@@ -70,19 +74,27 @@ struct MyPageView: View {
         // 로그인 여부에 대한 분기 처리
         VStack(alignment: .leading) {
             if let user = FirebaseAuthManager.shared.currentUser {
-                // FIXME: - 디자인 수정 (폰트, 컬러)
-                Text(user.name)
-                    .font(Font.system(size: 24, weight: .semibold))
-                    .foregroundStyle(Color.black)
-                Spacer().frame(height: 8)
-                Text(user.department)
-                    .font(Font.system(size: 16))
-                    .foregroundStyle(Color.gray)
-                Text(user.studentId)
-                    .font(Font.system(size: 16))
-                    .foregroundStyle(Color.gray)
+                Button {
+
+                } label: {
+                    VStack(alignment: .leading) {
+                        // FIXME: - 디자인 수정 (폰트, 컬러)
+                        Text(user.name)
+                            .font(Font.system(size: 24, weight: .semibold))
+                            .foregroundStyle(Color.black)
+                        Text(user.department)
+                            .font(Font.system(size: 16))
+                            .foregroundStyle(Color.gray)
+                        Text(user.studentId)
+                            .font(Font.system(size: 16))
+                            .foregroundStyle(Color.gray)
+                    } //: VSTACK
+                    .contentShape(Rectangle()) // 전체 폭 터치
+                }
+                .buttonStyle(.plain)// List에서 안전하게 동작
             } else {
                 Button {
+                    print("로그인")
                     router.push(to: .emailVerificationView)
                 } label: {
                     HStack(spacing: .zero) {
@@ -93,7 +105,9 @@ struct MyPageView: View {
                             .foregroundStyle(Color.black)
                             .frame(width: 24, height: 24)
                     }
+                    .contentShape(Rectangle()) // 전체 폭 터치
                 }
+                .buttonStyle(.plain)// List에서 안전하게 동작
             }
         }
     }
@@ -102,13 +116,16 @@ struct MyPageView: View {
     // MARK: - 리스트 header 아이템
     @ViewBuilder
     private func headerView(_ headerText: String) -> some View {
-        VStack {
-            Text(headerText)
-                .font(Font.system(size: 18, weight: .semibold)) // FIXME: - 폰트 수정
-                .foregroundStyle(Color.black) // FIXME: - Gray 700 수정
-                .offset(x: -20) // FIXME: - 더 좋은 방법으로 수정하기
-            Spacer().frame(height: 8)
-        } //: VSTACK
+        if headerText == "" {
+            EmptyView()
+        } else {
+            VStack {
+                Text(headerText)
+                    .font(Font.system(size: 18, weight: .semibold)) // FIXME: - 폰트 수정
+                    .foregroundStyle(Color.black) // FIXME: - Gray 700 수정
+                Spacer().frame(height: 8)
+            }
+        }
     }
     
     
@@ -125,20 +142,20 @@ struct MyPageView: View {
                     .settingListItem(color: Color.black) // FIXME: - Gray700 수정
             }
         case .action(let title, let action):
-            Button {
-                action()
-            } label: {
-                LabeledContent {
-                    Image(systemName: "chevron.right")
-                        .foregroundStyle(Color.gray) // FIXME: - Gray 500 수정
-                        .frame(width: 24, height: 24)
-                } label: {
+            Button(action: action) {
+                HStack {
                     Text(title)
-                        .settingListItem(
-                            color: title == "회원 탈퇴" ? Color.red : Color.black // FIXME: - Accent red, Gray700 수정
-                        )
+                        .font(.system(size: 16, weight: .medium))
+                        .foregroundStyle(title == "회원 탈퇴" ? .red : .primary)
+                    Spacer()
+                    Image(systemName: "chevron.right")
+                        .foregroundStyle(.gray)
+                        .frame(width: 24, height: 24)
                 }
+                .padding(.vertical, 20)
+                .contentShape(Rectangle()) // 전체 폭 터치
             }
+            .buttonStyle(.plain)// List에서 안전하게 동작
         }
     }
 }
