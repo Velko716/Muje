2400fbfe69bd69a7a8dc59fffb0e5f8f56d2d72e
Velko <wlsgurrla716@naver.com>
2025-08-26 20:35:19 +0900

    ✨[Feat]#17 - 신고 데이터 불러오기 불러오기
    현재 로그인 id를 기준으로 불러오기

--- Muje/Muje/Common/Enum/DTO/ReportType.swift
+++ Muje/Muje/Common/Enum/DTO/ReportType.swift
@@ -8,24 +8,9 @@
 import Foundation
 
 enum ReportType: String, Codable {
-    case spam = "spam"
-    case inappropriateContent = "inappropriateContent"
-    case harassment = "harassment"
-    case fraud = "fraud"
-    case other = "other"
-    
-    var displayName: String {
-        switch self {
-        case .spam:
-            return "스팸/도배"
-        case .inappropriateContent:
-            return "부적절한 내용"
-        case .harassment:
-            return "괴롭힘/욕설"
-        case .fraud:
-            return "사기/허위 정보"
-        case .other:
-            return "기타"
-        }
-    }
+    case spam = "스팸/도배"
+    case inappropriateContent = "부적절한 내용"
+    case harassment = "괴롭힘/욕설"
+    case fraud = "사기/허위 정보"
+    case other = "기타"
 }
---
+++ Muje/Muje/Modules/MyPage/ViewModels/Community/ReportsHistoryViewModel.swift
@@ -0,0 +1,52 @@
+//
+//  ReportsHistoryViewModel.swift
+//  Muje
+//
+//  Created by 김진혁 on 8/26/25.
+//
+
+import Foundation
+
+@Observable
+final class ReportsHistoryViewModel {
+    
+    var reports: [Report] = []
+    var isLoading: Bool = false
+        
+    // MARK: - report 데이터 불러오기
+    func loadReportData() async {
+        print("currentUser: \(FirebaseAuthManager.shared.currentUser?.userId ?? "없음")")
+        isLoading = true
+        do {
+            print("rawValue: \(Report.CodingKeys.reporterUserId.rawValue)")
+            let reports: [Report] = try await FirestoreManager.shared.fetchAll(
+                FirebaseAuthManager.shared.currentUser?.userId ?? "",
+                from: .reports,
+                where: Report.CodingKeys.reporterUserId.rawValue,
+                orderBy: Report.CodingKeys.createdAt.rawValue,
+            )
+            self.reports = reports
+            self.isLoading = false
+        } catch {
+            print("error: \(error.localizedDescription)")
+            self.isLoading = false
+        }
+    }
+    
+    // MARK: - 신고 생성 테스트 버튼 로직 (삭제 예정)
+    func createReportTestButtonTapped() async {
+        // 공고 신고 Model postId: "00DD1837-4416-46BD-8246-50CE26567717"
+        let report = Report(
+            reportId: UUID(),
+            reporterUserId: FirebaseAuthManager.shared.currentUser?.userId ?? "",
+            reportedUserId: "FTMIffTVLdb8GuhHweHJEgAwFqB2",
+            postId: "00DD1837-4416-46BD-8246-50CE26567717",
+            reportType: ReportType.spam.rawValue,
+            reason: "저한테 욕설을 빈번하게 사용했어요",
+            status: ReportStatus.pending.rawValue
+        )
+        
+        let _ = try? await FirestoreManager.shared.create(report)
+    }
+    
+}
