5071a1a83905c912be55d54a251ce8e6f35c6913
Velko <wlsgurrla716@naver.com>
2025-08-26 14:55:19 +0900

    ♻️[Refactor]#17 - Report DTO 수정

--- Muje/Muje/Common/Enum/DTO/ReportType.swift
+++ Muje/Muje/Common/Enum/DTO/ReportType.swift
@@ -8,7 +8,9 @@
 import Foundation
 
 enum ReportType: String, Codable {
-    case spam = "spam"
-    case abuse = "abuse"
-    case inappropriate = "inappropriate"
+    case spam = "스팸/도배"
+    case inappropriateContent = "부적절한 내용"
+    case harassment = "괴롭힘/욕설"
+    case fraud = "사기/허위 정보"
+    case other = "기타"
 }
