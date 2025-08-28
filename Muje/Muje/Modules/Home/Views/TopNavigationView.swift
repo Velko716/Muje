35cf562a3eedddea17f6d44f14f08994c9e6374d
Seo <seohyun6361@gmail.com>
2025-08-28 20:47:01 +0900

    💄[Design]#3 - 홈화면 ListStyle 수정

---
+++ Muje/Muje/Modules/Home/Views/TopNavigationView.swift
@@ -0,0 +1,51 @@
+//
+//  TopNavigationView.swift
+//  Muje
+//
+//  Created by 김서현 on 8/28/25.
+//
+
+import SwiftUI
+
+struct TopNavigationView: View {
+    @EnvironmentObject var router: NavigationRouter
+    
+    var body: some View {
+        HStack {
+            Text("oo대학교 모임 찾기")
+                .font(.headline)
+                .font(.system(size: 22))
+            Spacer()
+            
+            //MARK: 검색 아이콘
+            Button(action: {
+                router.push(to: .searchView)
+            }) {
+                Image(systemName: "magnifyingglass")
+                    .font(.system(size: 26))
+            }
+            
+            //MARK: 알림 아이콘
+            Button(action: {
+                router.push(to: .notificationView)
+            }) {
+                Image(systemName: "bell")
+                    .font(.system(size: 26))
+            }
+            
+            //MARK: 설정 아이콘
+            Button(action: {
+                router.push(to: .notificationView) //FIXME: 설정뷰로 수정
+            }) {
+                Image(systemName: "gearshape")
+                    .font(.system(size: 26))
+            }
+        } //: HSTACK
+        .padding(.bottom, 25)
+        .padding(.top, 20)
+    }
+}
+
+#Preview {
+    TopNavigationView()
+}
