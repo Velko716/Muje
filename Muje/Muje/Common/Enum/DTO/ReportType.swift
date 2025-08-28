9d7357d18331d66402f6c525e96b9c9124ca751f
Velko <wlsgurrla716@naver.com>
2025-08-29 01:14:21 +0900

    ✨[Feat]#17 - 신고 Enum DTO에 맞게 재수정

--- Muje/Muje/Common/Enum/DTO/ReportType.swift
+++ Muje/Muje/Common/Enum/DTO/ReportType.swift
@@ -8,9 +8,12 @@
 import Foundation
 
 enum ReportType: String, Codable {
-    case spam = "스팸/도배"
-    case inappropriateContent = "부적절한 내용"
-    case harassment = "괴롭힘/욕설"
-    case fraud = "사기/허위 정보"
+    case insult = "욕설 및 비하 (종교, 장애, 성별 등)"
+    case hateSpeech = "폭력 및 혐오표현"
+    case sexualContent = "음란물 및 성적인 표현"
+    case gambling = "도박 및 사행성 조장"
+    case spamAds = "스팸 및 광고"
+    case personalInfoLeak = "개인정보 유포"
+    case impersonationOrFalse = "사칭 및 허위 정보"
     case other = "기타"
 }
