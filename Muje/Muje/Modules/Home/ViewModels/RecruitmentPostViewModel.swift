ac496e107e60d1d80cfd10764402bc7b96d6de52
jHoon99 <eldkf1597@naver.com>
2025-08-30 18:08:41 +0900

    ✨[Feat]#33 - 지원제출항목, 커스텀질문 저장 로직

--- Muje/Muje/Modules/Home/ViewModels/RecruitmentPostViewModel.swift
+++ Muje/Muje/Modules/Home/ViewModels/RecruitmentPostViewModel.swift
@@ -11,22 +11,66 @@ import SwiftUI
 @Observable
 class RecruitmentPostViewModel {
     var basicInfoChecked: [RecruitmentField: Bool] = [:]
-    var customQuestions: [UUID: String] = [:]
+    var customQuestions: [DraftCustomQuestion] = []
+  
     var canSubmit: Bool {
-        let basicInfoCount = basicInfoChecked.values.filter {$0}.count
-        let customQuestionCount = customQuestions.values.filter { !$0.isEmpty }.count
-        return basicInfoCount >= 1 && customQuestionCount >= 1
+      let hasBasicInfo = basicInfoChecked.values.contains(true)
+      let validQuestionCount = customQuestions.filter {
+        !$0.questionText.trimmingCharacters(in: .whitespaces).isEmpty
+      }.count
+      return hasBasicInfo && validQuestionCount >= 1
     }
-    
+  
+  init() {
+    RecruitmentField.allCases.forEach { field in
+      basicInfoChecked[field] = field == .name
+    }
+  }
     //TODO: 뷰모델에서 개수 세긴 하는데 뷰에서 연결을 안햇네욤;;ㅎㅎ
     
     var postButtonColor: Color {
         canSubmit ? Color.black : Color.gray
     }
-    
-    init() {
-        RecruitmentField.allCases.forEach { field in
-            basicInfoChecked[field] = field == .name
-        }
+  
+  // MARK: - CustomQuestion 관련 로직
+  func addCustomQuestion() {
+    let newQue = DraftCustomQuestion(
+      questionText: "",
+      questionOrder: customQuestions.count + 1
+    )
+    customQuestions.append(newQue)
+  }
+  
+  func updateCustomQuestion(id: UUID, text: String) {
+    if let index = customQuestions.firstIndex(where: { $0.id == id }) {
+      customQuestions[index].questionText = text
     }
+  }
+  
+  func removeCustomQuestion(id: UUID) {
+    customQuestions.removeAll { $0.id == id }
+    
+    for index in customQuestions.indices {
+      customQuestions[index].questionOrder = index + 1
+    }
+  }
+  
+  // MARK: - Firebase Create
+  func createCustomQuestion(for postId: String) -> [CustomQuestion] {
+    return customQuestions.enumerated().compactMap { index, question in
+      guard question.isValid else { return nil }
+      
+      return CustomQuestion(questionId: UUID(), postId: postId, questionText: question.questionText, questionOrder: index + 1)
+    }
+  }
+}
+
+struct DraftCustomQuestion: Identifiable {
+  let id = UUID()
+  var questionText: String = ""
+  var questionOrder: Int
+  
+  var isValid: Bool {
+    !questionText.trimmingCharacters(in: .whitespaces).isEmpty
+  }
 }
