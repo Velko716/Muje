b009a85752c47474cb60d8ff0da5f2c5c798e441
Velko <wlsgurrla716@naver.com>
2025-08-31 20:30:27 +0900

    💄[Design]#36 - 회원가입 뷰 디자인 수정
    수정된 피그마 디자인에 맞춰 수정완료

--- Muje/Muje/Modules/MyPage/Views/Login/EmailVerificationView .swift
+++ Muje/Muje/Modules/MyPage/Views/Login/EmailVerificationView .swift
@@ -13,41 +13,77 @@ struct EmailVerificationView : View {
     @State private var emailText: String = ""
     
     var body: some View {
-        VStack {
-            Spacer()
-            TitleView
-            Spacer().frame(height: 20)
-            InputEmailTextFieldView
-            Spacer()
-            bottomButtonView
-            Spacer()
-        }
-        .paddingH16()
-    }
-    
-    
-    // MARK: - 이메일 인증 타이틀 뷰
-    private var TitleView: some View {
-        VStack(alignment: .leading) {
-            Text("학교 인증을 위해서\n대학교 이메일을 입력해주세요.(1/3)")
-                .font(.largeTitle)
-                .bold()
-        }
-    }
-    
-    // MARK: - 이메일 입력 텍스트 필드 뷰
-    private var InputEmailTextFieldView: some View {
-        VStack(alignment: .leading) {
-            Text("학교 이메일")
-                .font(.caption)
-                .bold()
-            
-            HStack(spacing: .zero) {
-                TextField("이메일을 입력해주세요.", text: $emailText)
-                Text("@jbnu.ac.kr")
-                    .font(.caption)
-                    .foregroundStyle(Color.gray)
+        ZStack {
+            Color.white
+            VStack {
+                topTitleView
+                Spacer().frame(height: 48)
+                middleInputEmailTextFieldView
+                Spacer()
             }
+            .paddingH16()
+        }
+        // TODO: 컴포넌트로 이 뷰를 만들기
+        .safeAreaInset(edge: .bottom) {
+            VStack {
+                BottomBar(
+                    text: "인증 요청",
+                    textColor: Color.white,
+                    bgColor: Color.black,
+                    enabled: emailText.isEmpty ? true : false
+                ) {
+                    viewModel.sendVerificationEmail(emailText: emailText)
+                }
+            }
+            .bottomBarBackground() // ViewModifier
+        }
+        .toolbar {
+            ToolbarLeadingBackButton()
+            ToolbarCenterTitle(text: "회원가입")
+        }
+    }
+    
+    
+    // MARK: - 탑 이메일 인증 타이틀 뷰
+    private var topTitleView: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(spacing: 10) {
+                    Circle()
+                        .fill(Color.blue) // FIXME: - 컬러 수정
+                        .frame(width: 24, height: 24)
+                        .overlay(alignment: .center) {
+                            Text("1")
+                                .font(Font.system(size: 14, weight: .semibold)) // FIXME: - 폰트 수정
+                                .foregroundStyle(Color.white) // FIXME: - 컬러 수정
+                                .monospacedDigit() // 숫자(0–9)만 고정 폭(tabular)으로 보이게 하는 SwiftUI 텍스트 모디파이어
+                                .frame(width: 24, height: 24, alignment: .center)
+                        }
+                    Circle()
+                        .fill(Color.gray)
+                        .frame(width: 10, height: 10)
+                }
+                Text("학교 인증을 위해서\n대학교 이메일을 입력해주세요.")
+                    .font(Font.system(size: 24, weight: .semibold)) // FIXME: - 폰트 수정
+                    .foregroundStyle(Color.black) // FIXME: - 컬러 수정
+            }
+            Spacer()
+        }
+    }
+    
+    
+    // MARK: - 미들 이메일 입력 텍스트 필드 뷰
+    private var middleInputEmailTextFieldView: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("대학교 이메일")
+                .font(Font.system(size: 14, weight: .semibold)) // FIXME: - 폰트 수정
+                .foregroundStyle(Color.black) // FIXME: - 컬러 수정
+                .offset(x: 4)
+            
+            RoundedTextField(
+                text: $emailText,
+                keyboard: .emailAddress
+            )
         }
     }
     
@@ -61,10 +97,10 @@ struct EmailVerificationView : View {
             }
         }
     }
-    
-    
 }
 
 #Preview {
-    EmailVerificationView()
+    NavigationStack {
+        EmailVerificationView()
+    }
 }
