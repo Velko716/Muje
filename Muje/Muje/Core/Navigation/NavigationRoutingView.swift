6c78353d2e6325b11580a2097bb6351252e8a99e
Velko <wlsgurrla716@naver.com>
2025-08-31 18:52:41 +0900

    🔧[Chore]#36 - StartLogin 네비게이션 추가

--- Muje/Muje/Core/Navigation/NavigationRoutingView.swift
+++ Muje/Muje/Core/Navigation/NavigationRoutingView.swift
@@ -62,6 +62,8 @@ struct NavigationRoutingView: View {
                 BlockHistoryView()
             case .textView(let type):
                 TextView(viewModel: TextViewModel(type: type)) // 커뮤니티 이용 규칙, 서비스 이용약관, 개인정보 처리 방침, 청소년 보호 정책, 오픈 소스 라이선스
+            case .startLoginView:
+                StartLoginView()
             }
         }
         .hideBackButton()
