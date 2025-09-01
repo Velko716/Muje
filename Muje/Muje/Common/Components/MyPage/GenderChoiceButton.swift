b5531c831774d5f6d6bcbf4d2301e535deb65fd1
Velko <wlsgurrla716@naver.com>
2025-09-02 04:35:49 +0900

    💄[Design]#36 - 성별 선택 버튼 애니메이션 추가
    버튼 선택 시 애니메이션 추가

--- Muje/Muje/Common/Components/MyPage/GenderChoiceButton.swift
+++ Muje/Muje/Common/Components/MyPage/GenderChoiceButton.swift
@@ -9,26 +9,32 @@ import SwiftUI
 
 struct GenderChoiceButton: View {
     let title: String
+    let isSelected: Bool
     let action: () -> Void
     
     var body: some View {
         Button(action: action) {
             RoundedRectangle(cornerRadius: 10)
-                .fill(Color.blue) // FIXME: - 컬러 수정
+                .fill(Color.blue.opacity(0.2)) // FIXME: - 컬러 수정
                 .frame(maxWidth: .infinity)
                 .overlay {
                     Text(title)
                         .font(Font.system(size: 18, weight: .semibold)) // FIXME: - 폰트 수정
-                        .foregroundStyle(Color.black) // FIXME: - 컬러 수정
+                        .foregroundStyle(isSelected ? Color.blue.opacity(0.6) : Color.black) // FIXME: - 컬러 수정
                 }
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(isSelected ? Color.blue : .clear, lineWidth: 1)
+                )
         }
+        .animation(.smooth(duration: 0.20), value: isSelected)
     }
 }
 
 #Preview(traits: .sizeThatFitsLayout) {
     HStack(spacing: 17) {
-        GenderChoiceButton(title: "남") {}
-        GenderChoiceButton(title: "여") {}
+        GenderChoiceButton(title: "남", isSelected: true) {}
+        GenderChoiceButton(title: "여", isSelected: false) {}
     }
     .frame(height: 56)
     .padding(.horizontal, 16)
