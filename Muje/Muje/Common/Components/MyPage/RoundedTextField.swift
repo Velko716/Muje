3e31cdcb68fdf124fbfd8d84f8c3028d96f2d37b
Velko <wlsgurrla716@naver.com>
2025-09-01 23:26:52 +0900

    ✨[Feat]#36 - 텍스트 필드 컴포넌트에 패스워드 분기 처리 구현

--- Muje/Muje/Common/Components/MyPage/RoundedTextField.swift
+++ Muje/Muje/Common/Components/MyPage/RoundedTextField.swift
@@ -9,32 +9,39 @@ import SwiftUI
 
 struct RoundedTextField: View {
     @Binding var text: String
+    var placeholder: String
     var keyboard: UIKeyboardType
+    var isSecure: Bool = false
     
     @FocusState private var isFocused: Bool
-        
+    
     var body: some View {
-        ZStack(alignment: .leading) {
-            TextField("@jbnu.ac.kr", text: $text)
-                .focused($isFocused)
-                .font(Font.system(size: 16, weight: .medium)) // FIXME: - 폰트 수정
-                .keyboardType(keyboard)
-                .padding(.horizontal, 16)
-                .frame(height: 62)
-                .background(
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Color.white)
-                )
-                .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(isFocused ? Color.blue : Color.gray, lineWidth: 1) // FIXME: - 컬러 수정하기
-                )
-                .animation(.easeOut(duration: 0.15), value: isFocused)
+        ZStack {
+            if isSecure {
+                SecureField(placeholder, text: $text)
+            } else {
+                TextField(placeholder, text: $text)
+            }
         }
+        .focused($isFocused)
+        .font(Font.system(size: 16, weight: .medium)) // FIXME: - 폰트 수정
+        .keyboardType(keyboard)
+        .padding(.horizontal, 16)
+        .frame(height: 62)
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+        )
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(isFocused ? Color.blue : Color.gray, lineWidth: 1) // FIXME: - 컬러 수정하기
+        )
+        .animation(.easeOut(duration: 0.15), value: isFocused)
+        
     }
 }
 
 #Preview {
-    RoundedTextField(text: .constant(""), keyboard: .emailAddress)
+    RoundedTextField(text: .constant(""), placeholder: "@jbnu.ac.kr", keyboard: .emailAddress)
         .padding(.horizontal, 16)
 }
