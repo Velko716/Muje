f1556663e9052966fd10994df799f5f61bf7a481
Velko <wlsgurrla716@naver.com>
2025-09-02 19:36:35 +0900

    💄[Design]#36 - UserInfoInputView 디자인 수정
    최신 업데이트 된 피그마에 맞춰 디자인 구현.
    폰트, 컬러는 미적용

--- Muje/Muje/Modules/MyPage/Views/Login/UserInfoInputView.swift
+++ Muje/Muje/Modules/MyPage/Views/Login/UserInfoInputView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct UserInfoInputView: View {
     
     @EnvironmentObject private var router: NavigationRouter
-//    @Bindable var emailVerificationVM: EmailVerificationViewModel
-//    @Bindable var userInfoVM: UserInfoInputViewModel
+    //    @Bindable var emailVerificationVM: EmailVerificationViewModel
+    //    @Bindable var userInfoVM: UserInfoInputViewModel
     
     let uuid: String
     let email: String
@@ -22,96 +22,226 @@ struct UserInfoInputView: View {
     @State private var department: String = ""
     @State private var studentId: String = ""
     
+    @State private var maleIsSelected: Bool = false
+    @State private var femaleIsSelected: Bool = false
     
     var body: some View {
-        VStack(alignment: .leading) {
-            topTitleView
-            Spacer().frame(height: 80)
-            middleInfoInputView
-            Spacer().frame(height: 80)
-            bottomNextButtonView
-        } //: VSTACK
-        .paddingH16()
-    }
-    
-    // MARK: - 상단 타이틀 뷰
-    private var topTitleView: some View {
-        VStack(alignment: .leading) {
-            Text("인적사항을 입력하세요.(2/3)")
+        ZStack {
+            Color.white
+            ScrollView(showsIndicators: false) {
+                VStack(alignment: .leading, spacing: .zero) {
+                    Spacer().frame(height: 32)
+                    topTitleView
+                    Spacer().frame(height: 48)
+                    middleInfoInputView
+                    Spacer().frame(height: 94)
+                }
+            }
+            .paddingH16()
+        }
+        .safeAreaInset(edge: .bottom) {
+            VStack {
+                BottomBar(
+                    text: "확인",
+                    textColor: .white,
+                    bgColor: .black,
+                    enabled: true
+                ) {
+                    let user = User(
+                        userId: self.uuid,
+                        email: self.email,
+                        name: name,
+                        birthYear: Int(birthYear) ?? 0,
+                        gender: gender,
+                        department: department,
+                        studentId: studentId,
+                        emailVerified: true,
+                        termsAgreed: true,
+                        privacyAgreed: true
+                    )
+                    
+                    Task {
+                        do {
+                            let _ = try await FirestoreManager.shared.update(user)
+                        } catch {
+                            print("error : \(error.localizedDescription)")
+                        }
+                        router.popToRootView() // FIXME: - 임시
+                    }
+                }
+            }
+            .padding(.bottom, 43)
+            .bottomBarBackground()
+        }
+        .toolbar {
+            ToolbarLeadingBackButton()
+            ToolbarCenterTitle(text: "회원가입")
         }
     }
+    
+    // MARK: - 상단
+    private var topTitleView: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 8) {
+                HStack(spacing: 10) {
+                    Circle().fill(Color.gray)
+                        .frame(width: 10, height: 10)
+                    Circle().fill(Color.blue)
+                        .frame(width: 24, height: 24)
+                        .overlay {
+                            Text("2")
+                                .font(.system(size: 14, weight: .semibold)) // FIXME: - 폰트 수정
+                                .foregroundStyle(.white) // FIXME: - 컬러 수정
+                        }
+                }
+                Text("인적사항을 입력해주세요")
+                    .font(.system(size: 24, weight: .semibold)) // FIXME: - 폰트 수정
+                    .foregroundStyle(.black) // FIXME: - 컬러 수정
+            }
+            Spacer()
+        }
+    }
+    
     
     // MARK: - 중간 유저 정보 입력 뷰
     private var middleInfoInputView: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading) {
             Text("이름")
-            TextField("이름을 입력해주세요.", text: $name)
+                .font(Font.system(size: 14, weight: .semibold))
+                .foregroundStyle(Color.black)
             
-            Text("출생년도")
-            TextField("출생년도를 입력해주세요.", text: $birthYear)
+            Spacer().frame(height: 8)
             
-            HStack(spacing: 8) {
-                Button {
-                    self.gender = Gender.male.rawValue
-                } label: {
-                    Text("남")
-                }
-                
-                Button {
-                    self.gender = Gender.female.rawValue
-                } label: {
-                    Text("여")
-                }
-            }
+            RoundedTextField(
+                text: $name,
+                placeholder: "이름을 입력해주세요",
+                keyboard: .emailAddress
+            )
+            
+            Spacer().frame(height: 32)
+            
+            Text("생년월일")
+                .font(Font.system(size: 14, weight: .semibold))
+                .foregroundStyle(Color.black)
+            
+            Spacer().frame(height: 8)
+            
+            RoundedTextField(
+                text: $birthYear,
+                placeholder: "생년월일 8자리를 입력해주세요",
+                keyboard: .emailAddress
+            )
+            
+            Spacer().frame(height: 32)
             
             Text("학과")
-            TextField("학과를 입력해주세요.", text: $department)
+                .font(Font.system(size: 14, weight: .semibold))
+                .foregroundStyle(Color.black)
+            
+            Spacer().frame(height: 8)
+            
+            RoundedTextField(
+                text: $department,
+                placeholder: "학과를 입력해주세요",
+                keyboard: .emailAddress
+            )
+            
+            Spacer().frame(height: 32)
             
             Text("학번")
-            TextField("학번을 입력해주세요.", text: $studentId)
-        }
-    }
-    
-    // MARK: - 바텀 다음 버튼 뷰
-    private var bottomNextButtonView: some View {
-        VStack {
-            Button {
-                let user = User(
-                    userId: self.uuid,
-                    email: self.email,
-                    name: name,
-                    birthYear: Int(birthYear) ?? 0,
-                    gender: gender,
-                    department: department,
-                    studentId: studentId,
-                    emailVerified: true,
-                    termsAgreed: true,
-                    privacyAgreed: true
-                )
-                
-                Task {
-                    do {
-                        let _ = try await FirestoreManager.shared.update(user)
-                    } catch {
-                        print("error : \(error.localizedDescription)")
-                    }
-                    router.popToRootView() // FIXME: - 임시 
+                .font(Font.system(size: 14, weight: .semibold))
+                .foregroundStyle(Color.black)
+            
+            Spacer().frame(height: 8)
+            
+            RoundedTextField(
+                text: $studentId,
+                placeholder: "학번을 입력해주세요",
+                keyboard: .emailAddress
+            )
+            
+            Spacer().frame(height: 32)
+            
+            Text("성별")
+                .font(Font.system(size: 14, weight: .semibold))
+                .foregroundStyle(Color.black)
+            
+            Spacer().frame(height: 8)
+            
+            HStack(spacing: 17) {
+                GenderChoiceButton(title: "남", isSelected: maleIsSelected) {
+                    maleIsSelected = true
+                    femaleIsSelected = false
+                    self.gender = Gender.male.rawValue
                 }
-            } label: {
-                Text("다음")
-                    .font(.subheadline)
-                    .foregroundStyle(Color.black)
-                    .bold()
-                    .background(
-                        Color.blue
-                            .padding()
-                    )
+                GenderChoiceButton(title: "여", isSelected: femaleIsSelected) {
+                    maleIsSelected = false
+                    femaleIsSelected = true
+                    self.gender = Gender.female.rawValue
+                }
             }
+            .frame(height: 56)
+            
+            Spacer().frame(height: 32)
+            
+            Text("약관동의")
+                .font(Font.system(size: 14, weight: .semibold))
+                .foregroundStyle(Color.black)
+            
+            Spacer().frame(height: 8)
+            
+            
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color.white)
+                .stroke(Color.gray, lineWidth: 1)
+                .overlay {
+                    VStack(spacing: 8) {
+                        Button {
+                            // TODO: 약관 동의 뷰 네비게이션 이동하기
+                        } label: {
+                            HStack(spacing: .zero) {
+                                Text("[필수] 이용약관에 동의합니다.")
+                                    .font(Font.system(size: 16, weight: .medium)) // FIXME: - 폰트 수정
+                                    .foregroundStyle(Color.gray) // FIXME: - 컬러 수정
+                                Image(.iconChevronRight)
+                                    .foregroundStyle(Color.gray) // FIXME: - 컬러 수정
+                                    .frame(width: 24, height: 24)
+                                Spacer()
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 1) // FIXME: - 테두리 색 수정
+                                    .fill(Color.white) // FIXME: - 컬러 수정
+                                    .frame(width: 26, height: 26) // FIXME: - 이미지로 수정
+                            }
+                        }
+                        Button {
+                            // TODO: 약관 동의 뷰 네비게이션 이동하기
+                        } label: {
+                            HStack(spacing: .zero) {
+                                Text("[필수] 개인정보 처리방침에 동의합니다.")
+                                    .font(Font.system(size: 16, weight: .medium)) // FIXME: - 폰트 수정
+                                    .foregroundStyle(Color.gray) // FIXME: - 컬러 수정
+                                Image(.iconChevronRight)
+                                    .foregroundStyle(Color.gray) // FIXME: - 컬러 수정
+                                    .frame(width: 24, height: 24)
+                                Spacer()
+                                Circle()
+                                    .stroke(Color.gray, lineWidth: 1) // FIXME: - 테두리 색 수정
+                                    .fill(Color.white) // FIXME: - 컬러 수정
+                                    .frame(width: 26, height: 26) // FIXME: - 이미지로 수정
+                            }
+                        }
+                    }
+                    .padding(16)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: 106)
         }
     }
 }
 
 #Preview {
-    UserInfoInputView(uuid: "", email: "")
-        .environmentObject(NavigationRouter())
+    NavigationStack {
+        UserInfoInputView(uuid: "", email: "")
+            .environmentObject(NavigationRouter())
+    }
 }
