3db6f91714b40d5082d23e74703fb95ce3c3ba6a
Velko <wlsgurrla716@naver.com>
2025-09-03 21:58:26 +0900

    🔧[Chore]#36 - Login 관련 폴더링 재구성
    뷰가 많아짐에 따라 폴더링을 재구성함.

---
+++ Muje/Muje/Modules/MyPage/Views/Login/SignIn/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Muje
+//
+//  Created by 김진혁 on 9/3/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    LoginView()
+}
