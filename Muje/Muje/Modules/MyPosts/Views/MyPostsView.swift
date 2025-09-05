6d33394bc0a64ad3fdc4c57e213640efa7672385
jHoon99 <eldkf1597@naver.com>
2025-09-05 15:58:53 +0900

    ✨[Feat]#51 - 공고 현황탭 중복 데이터 로드 수정
    * 병렬 구조 변경, 캐싱 방식 적용
    > 새로고침할때 뷰에서 크래시나는 현상 있음 확인 필요

--- Muje/Muje/Modules/MyPosts/Views/MyPostsView.swift
+++ Muje/Muje/Modules/MyPosts/Views/MyPostsView.swift
@@ -19,8 +19,15 @@ struct MyPostsView: View {
               applyView
           }
         }
-        .task {
-          await myPostsViewModel.loadAllData()
+        .onAppear {
+          Task {
+            await myPostsViewModel.loadAllDataIfNeed()
+          }
+        }
+        .refreshable {
+          Task {
+            await myPostsViewModel.forceRefresh()
+          }
         }
 //        .onAppear {
 //          Task {
