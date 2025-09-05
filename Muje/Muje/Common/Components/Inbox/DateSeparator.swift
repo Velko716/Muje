afdd72a42028d4d25a2ed95ba3736641eee681e1
Velko <wlsgurrla716@naver.com>
2025-09-05 15:56:10 +0900

    💄[Design]#53 - 날짜선 구분 컴포넌트 디자인 수정
    폰트, 컬러, 패딩 수정

--- Muje/Muje/Common/Components/Inbox/DateSeparator.swift
+++ Muje/Muje/Common/Components/Inbox/DateSeparator.swift
@@ -7,16 +7,19 @@
 
 import SwiftUI
 
+/// 날짜 구분선 컴포넌트 입니다
 struct DateSeparator: View {
     let date: Date
     var body: some View {
         HStack {
-            Rectangle().fill(.secondary.opacity(0.2)).frame(height: 1)
+            Rectangle().fill(Color.gray400).frame(height: 0.5)
+            Spacer()
             Text("\(date.fullDateWeekday(date))")
-                .font(.caption).foregroundStyle(.secondary)
-                .padding(.horizontal, 7)
+                .font(Font.pretendard(type: .medium, size: 12))
+                .foregroundStyle(Color.gray400)
                 .fixedSize(horizontal: true, vertical: false)
-            Rectangle().fill(.secondary.opacity(0.2)).frame(height: 1)
+            Spacer()
+            Rectangle().fill(Color.gray400).frame(height: 0.5)
         }
         .padding(.bottom, 12)
     }
@@ -25,4 +28,5 @@ struct DateSeparator: View {
 
 #Preview {
     DateSeparator(date: Date())
+        .padding(.horizontal, 16)
 }
