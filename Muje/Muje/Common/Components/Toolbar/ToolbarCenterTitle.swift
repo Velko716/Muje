897b93d09454f9ec49c792cc6b6089229e4c4f5c
Velko <wlsgurrla716@naver.com>
2025-09-05 15:53:17 +0900

    💄[Design]#53 - common ToolbarCenterText 컴포넌트 디자인 수정
    폰트, 컬러 수정

--- Muje/Muje/Common/Components/Toolbar/ToolbarCenterTitle.swift
+++ Muje/Muje/Common/Components/Toolbar/ToolbarCenterTitle.swift
@@ -13,8 +13,8 @@ struct ToolbarCenterTitle: ToolbarContent {
     var body: some ToolbarContent {
         ToolbarItem(placement: .principal) {
             Text(text)
-                .font(Font.system(size: 18, weight: .semibold))
-                .foregroundStyle(Color.black)
+                .font(Font.pretendard(type: .semiBold, size: 18))
+                .foregroundStyle(Color.gray700)
                 .allowsHitTesting(false) // FIXME: - 가끔 리스트 버튼의 터치영역을 방해하는 요소를 제거하기 위해 구현 (존재 여부에 대해서 이야기)
         }
     }
