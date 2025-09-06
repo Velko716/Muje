d1d82d7b84ab3dc008177f5cdc22579351864f78
Seo <seohyun6361@gmail.com>
2025-09-06 17:10:06 +0900

    🔧[Chore]#55 - 바텀버튼 컴포넌트 구현

--- Muje/Muje/Common/Components/Custom/TwoActionBottomButton.swift
+++ Muje/Muje/Common/Components/Custom/TwoActionBottomButton.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct TwoActionBottomButton: View {
+    var leftAction: () -> Void
+    var leftText: String
+    var leftBGColor: Color = .gray50
+    var rightAction: () -> Void
+    var rightText: String
+    var rightBGColor: Color = .primaryBlack
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 17) {
+            Button {
+                leftAction()
+            } label: {
+                Text(leftText)
+                    .font(.system(size: 18))
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16)
+                    .background(Color.black)
+                    .foregroundStyle(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+            Button {
+                rightAction()
+            } label: {
+                Text(rightText)
+                    .font(.system(size: 18))
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 16)
+                    .background(rightBGColor)
+                    .foregroundStyle(.white01)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+        }
+        .padding(EdgeInsets(top: 20, leading: 16, bottom: 43, trailing: 16))
+        .background(
+            Rectangle()
+                .fill(.white)
+                .shadow(color: .black.opacity(0.1), radius: 8, x: 0, y: -4)
+                .ignoresSafeArea(.all, edges: .bottom)
+        )
     }
-}
-
-#Preview {
-    TwoActionBottomButton()
 }
