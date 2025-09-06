875bb61d574c823293b71d02818544be9a2b649a
Seo <seohyun6361@gmail.com>
2025-09-06 17:09:04 +0900

    🔧[Chore]#55 - 신청서 제출 완료 화면 구현

--- Muje/Muje/Modules/Home/Views/ApplicationPreView/ApplicationSubmitCompleteView.swift
+++ Muje/Muje/Modules/Home/Views/ApplicationPreView/ApplicationSubmitCompleteView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ApplicationSubmitCompleteView: View {
+    @EnvironmentObject private var router: NavigationRouter
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            TopTextView
+            Spacer()
+            graphicsView
+            Spacer()
+        }
+        .toolbar {
+            ToolbarLeadingBackButton()
+            ToolbarCenterTitle(text: "신청서 작성")
+        }
+        TwoActionBottomButton(
+            leftAction: { router.push(to: .myPostView) } ,
+            leftText: "나의 모임페이지",
+            rightAction: { router.push(to: .contentView) },
+            rightText: "홈으로"
+        )
     }
+    private var TopTextView: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 10) {
+                Text("신청서가 제출되었어요!")
+                    .headline24SemiBold()
+                    .foregroundStyle(.gray700)
+                Text("모집자에게 신청이 전달되었어요\n합격 발표 알림을 기다려주세요")
+                    .body1Regular16()
+                    .foregroundStyle(.gray500)
+            }
+            .padding(.horizontal, 16)
+            .padding(.top, 24)
+            Spacer()
+        }
+    }
+    
+    private var graphicsView: some View {
+        Image(.temp)
+            .resizable()
+            .scaledToFit()
+            .padding(.horizontal, 79.5)
+    }
+
 }
 
 #Preview {
