3cd3759ab4d2433f6c70ef3146f036a799359d71
Seo <seohyun6361@gmail.com>
2025-09-06 17:07:56 +0900

    🔧[Chore]#55 - 바텀버튼 컴포넌트 추가
    액션 2개 있는 경우

---
+++ Muje/Muje/Common/Components/Custom/TwoActionBottomButton.swift
@@ -0,0 +1,18 @@
+//
+//  TwoActionBottomButton.swift
+//  Muje
+//
+//  Created by 김서현 on 9/6/25.
+//
+
+import SwiftUI
+
+struct TwoActionBottomButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TwoActionBottomButton()
+}
