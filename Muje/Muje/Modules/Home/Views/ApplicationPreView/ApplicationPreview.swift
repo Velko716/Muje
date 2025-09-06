bae3fcfaaadef9a395868966d74d75e6dc21236a
Seo <seohyun6361@gmail.com>
2025-09-06 15:30:54 +0900

    💄[Design]#55 - 프리뷰 디자인 수정
    패딩 값 조절, 폰트, 컬러 수정

--- Muje/Muje/Modules/Home/Views/ApplicationPreView/ApplicationPreview.swift
+++ Muje/Muje/Modules/Home/Views/ApplicationPreView/ApplicationPreview.swift
@@ -47,64 +47,105 @@ struct ApplicationPreview: View {
       await viewModel.loadUserData(userId: userId)
     }
     
-    bottomButtonSection
+    @EnvironmentObject private var router: NavigationRouter
     
-  }
-  
-  private var userInfoSection: some View {
-    VStack(alignment: .leading) {
-      Text(viewModel.userInfo?.name ?? "")
-      
-      HStack {
-        if requirementFlags.requiresGender {
-          Text(viewModel.userInfo?.genderDisplay ?? "")
+    let postId: String // 키체인 구현전까지 테스트용으로 userId로 같이 씀.
+    let requirementFlags: RequirementFlags
+    let postBasicInfo: PostBasicInfo
+    let customQuestion: [CustomQuestion]
+    @Binding var questionAnswer: [String: String]
+    
+    @State private var viewModel = ApplicationPreviewModel()
+    
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            CustomNavigationBar(
+                title: "신청서 미리보기") {
+                    router.pop()
+                }
+            ScrollView {
+                
+                userInfoSection
+                Spacer().frame(height: 24)
+                userInfoDetailSection
+                Spacer().frame(height: 24)
+                Rectangle()
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 12)
+                    .foregroundStyle(Color.gray.opacity(0.2)) //TODO: divider 컴포넌트로 변경
+                Spacer().frame(height: 24)
+                customQuestionSection
+                
+            }
         }
-        if requirementFlags.requiresAge {
-          Text(viewModel.userInfo?.ageString ?? "")
+        .task {
+            await viewModel.loadUserData(userId: postId)
         }
-      }
+        
+        bottomButtonSection
+        
     }
-    .frame(maxWidth: .infinity, alignment: .leading)
-    .padding(.horizontal, 24)
-    .padding(.top, 16)
-    .padding(.bottom, 24)
-  }
-  
-  private var userInfoDetailSection: some View {
-    VStack(spacing: 5) {
-      if requirementFlags.requiresStudentId {
-        InfoRow(title: "학번", value: viewModel.userInfo?.studentId ?? "")
-      }
-      if requirementFlags.requiresDepartment {
-        InfoRow(title: "학과", value: viewModel.userInfo?.department ?? "")
-      }
+    
+    private var userInfoSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(viewModel.userInfo?.name ?? "")
+                .headline24SemiBold()
+            
+            HStack(spacing: 0) {
+                if requirementFlags.requiresGender && requirementFlags.requiresAge {
+                    Text(viewModel.userInfo?.genderDisplay ?? "")
+                        .body2Regular16()
+                        .foregroundStyle(.gray800)
+                    Text(", ")
+                        .body2Regular16()
+                        .foregroundStyle(.gray800)
+                    Text(viewModel.userInfo?.ageString ?? "")
+                        .body2Regular16()
+                        .foregroundStyle(.gray800)
+                }
+                else if requirementFlags.requiresGender && !requirementFlags.requiresAge {
+                    Text(viewModel.userInfo?.genderDisplay ?? "")
+                        .body2Regular16()
+                        .foregroundStyle(.gray800)
+                }
+                else if !requirementFlags.requiresAge && requirementFlags.requiresAge {
+                    Text(viewModel.userInfo?.ageString ?? "")
+                        .body2Regular16()
+                        .foregroundStyle(.gray800)
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding(.horizontal, 16)
+        .padding(.top, 16)
     }
-    .padding(.horizontal, 24)
-    .padding(.bottom, 24)
-  }
-  
-  private var customQuestionSection: some View {
-    VStack {
-      ForEach(customQuestion, id: \.questionId) { question in
-        QuestionAnswerToggle(
-          question: question.questionText,
-          answer: questionAnswer[question.questionId.uuidString] ?? "답변이 입력되지 않았습니다."
-        )
-      }
+    
+    private var userInfoDetailSection: some View {
+        VStack(spacing: 5) {
+            if requirementFlags.requiresStudentId {
+                InfoRow(title: "학번", value: viewModel.userInfo?.studentId ?? "")
+            }
+            if requirementFlags.requiresDepartment {
+                InfoRow(title: "학과", value: viewModel.userInfo?.department ?? "")
+            }
+        }
+        .padding(.horizontal, 24)
     }
-  }
-  
-  private var bottomButtonSection: some View {
-    VStack {
-      Button {
-        Task {
-          try await viewModel.submitApplication(
-            postId: postId,
-            post: postBasicInfo,
-            requirement: requirementFlags,
-            questionAnswer: questionAnswer,
-            customQuestion: customQuestion
-          )
+    
+    private var customQuestionSection: some View {
+        VStack {
+            ForEach(customQuestion, id: \.questionId) { question in
+                QuestionAnswerToggle(
+                    question: question.questionText,
+                    answer: questionAnswer[question.questionId.uuidString] ?? "답변이 입력되지 않았습니다."
+                )
+                if question.questionId != customQuestion.last?.questionId {
+                    Divider()
+                        .foregroundStyle(.gray50)
+                        .padding(.horizontal, 16)
+                }
+            }
         }
         router.push(to: .applicationCompleteView)
       } label: {
@@ -116,84 +157,29 @@ struct ApplicationPreview: View {
           .clipShape(RoundedRectangle(cornerRadius: 10))
       }
     }
-    .padding(.vertical, 32)
-    .padding(.horizontal, 16)
-  }
-}
-
-#Preview {
-  @Previewable @State var viewModel = ApplicationPreviewModel()
     
-  return ApplicationPreview(
-    postId: "",
-    requirementFlags: RequirementFlags(
-      from: Post(
-        postId: UUID(),
-        authorUserId: "",
-        title: "ddddd",
-        organization: "dddddd",
-        content: "ddddddd",
-        recruitmentStart: Timestamp(date: Date()),
-        recruitmentEnd: Timestamp(date: Date()),
-        hasInterview: true,
-        status: PostStatus.recruiting.rawValue,
-        requiresName: true,
-        requiresStudentId: true,
-        requiresDepartment: true,
-        requiresGender: true,
-        requiresAge: true,
-        requiresPhone: true,
-        authorName: "박기연",
-        authorOrganization: "MAD"
-      )
-    ),
-    postBasicInfo: PostBasicInfo(
-      from: Post(
-        postId: UUID(),
-        authorUserId: "",
-        title: "ddddd",
-        organization: "dddddd",
-        content: "ddddddd",
-        recruitmentStart: Timestamp(date: Date()),
-        recruitmentEnd: Timestamp(date: Date()),
-        hasInterview: true,
-        status: PostStatus.recruiting.rawValue,
-        requiresName: true,
-        requiresStudentId: true,
-        requiresDepartment: true,
-        requiresGender: true,
-        requiresAge: true,
-        requiresPhone: true,
-        authorName: "박기연",
-        authorOrganization: "MAD"
-      )
-    ),
-    customQuestion: [CustomQuestion(
-      questionId: UUID(),
-      postId: "",
-      questionText: "햄버거 햄버거",
-      questionOrder: 1
-    ),CustomQuestion(
-      questionId: UUID(),
-      postId: "",
-      questionText: "햄버거 햄버거",
-      questionOrder: 2
-    ),CustomQuestion(
-      questionId: UUID(),
-      postId: "",
-      questionText: "햄버거 햄버거",
-      questionOrder: 3
-    ),CustomQuestion(
-      questionId: UUID(),
-      postId: "",
-      questionText: "햄버거 햄버거",
-      questionOrder: 4
-    ),CustomQuestion(
-      questionId: UUID(),
-      postId: "",
-      questionText: "햄버거 햄버거",
-      questionOrder: 5
-    )],
-    questionAnswer: .constant(["dddddddddd" : "우아아아아ㅏㅇ아ㅏ아"])
-  )
+    private var bottomButtonSection: some View {
+        VStack {
+            Button {
+                Task {
+                    try await viewModel.submitApplication(
+                        postId: postId,
+                        post: postBasicInfo,
+                        requirement: requirementFlags,
+                        questionAnswer: questionAnswer,
+                        customQuestion: customQuestion
+                    )
+                }
+            } label: {
+                Text("확인")
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 18)
+                    .background(Color.gray)
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+            }
+        }
+        .padding(.vertical, 32)
+        .padding(.horizontal, 16)
+    }
 }
