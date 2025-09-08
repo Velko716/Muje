b7f7e72d1e927d38e677aa7daab779bc7d34d7e9
Seo <seohyun6361@gmail.com>
2025-09-09 02:22:40 +0900

    💄[Design]#65 - CustomInput 디자인 적용
    폰트, 컬러 수정

--- Muje/Muje/Modules/UploadPost/Component/CustomInput.swift
+++ Muje/Muje/Modules/UploadPost/Component/CustomInput.swift
@@ -8,32 +8,46 @@
 import SwiftUI
 
 struct CustomInput: View {
+    @FocusState private var isFocused: Bool
     var title: String
     var tempTitle: String
     var textValue: Binding<String>
     var subTitle: String?
     let maxLength: Int
-    
+    let config = Font.lineHeight(
+        type: .medium,
+        fontSize: 16,
+        lineHeightPercent: 1.85,
+        letterSpacingPercent: -1
+    )
     var body: some View {
-        VStack(alignment: .leading) {
+        VStack(alignment: .leading, spacing: 8) {
             if let sub = subTitle {
-                HStack(spacing: 4) {
+                HStack(alignment: .bottom, spacing: 8) {
                     Text(title)
+                        .caption14SemiBold()
+                        .foregroundStyle(.gray700)
                     Text(sub)
-                        .font(.caption)
-                        .foregroundStyle(Color.gray)
+                        .caption14Regular()
+                        .foregroundStyle(.gray500)
                 }
             } else {
                 Text(title)
+                    .caption14SemiBold()
+                    .foregroundStyle(.gray700)
             }
             TextField(tempTitle, text: textValue, axis: .vertical)
                 .maxLength(text: textValue, maxLength)
-                .bold()
-                .padding(18)
+                .focused($isFocused)
+                .font(.pretendard(type: .medium, size: 16))
+                .padding(.vertical, config.verticalPadding)
+                .padding(16)
+                .tracking(config.letterSpacing)
+                .foregroundStyle(.gray700, .gray500)
                 .background(
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.clear)
-                        .strokeBorder(Color.gray.opacity(0.2), lineWidth: 1)
+                        .strokeBorder(isFocused ? .pointSkyBlue : .gray100, lineWidth: 1)
                 )
         }
     }
