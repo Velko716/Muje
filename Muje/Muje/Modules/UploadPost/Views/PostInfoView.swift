195262108ba097e1b07a24449627ff02dfa20d3f
Seo <seohyun6361@gmail.com>
2025-09-09 02:23:40 +0900

    💄[Design]#65 - PostInfoView 디자인 반영
    컬러, 폰트 수정

--- Muje/Muje/Modules/UploadPost/Views/PostInfoView.swift
+++ Muje/Muje/Modules/UploadPost/Views/PostInfoView.swift
@@ -10,12 +10,17 @@ import PhotosUI
 
 struct PostInfoView: View {
     @Bindable var postInfoViewModel: PostInfoViewModel
-    
+    let config = Font.lineHeight(
+        type: .medium,
+        fontSize: 16,
+        lineHeightPercent: 1.85,
+        letterSpacingPercent: -1
+    )
     
     var body: some View {
         ZStack {
             VStack(spacing: 32) {
-                CustomInput(title: "공고제목", tempTitle: "공고 제목을 적어주세요", textValue: $postInfoViewModel.title, maxLength: 100)
+                CustomInput(title: "모집글 제목", tempTitle: "공고 제목을 적어주세요", textValue: $postInfoViewModel.title, maxLength: 100)
                 CustomInput(title: "단체명", tempTitle: "단체명을 적어주세요", textValue: $postInfoViewModel.organization, maxLength: 50)
                 
                 PostDatePicker(title: "모집 마감일", content: postInfoViewModel.endDateString, function: {
@@ -31,26 +36,42 @@ struct PostInfoView: View {
     
     private var imageView: some View {
         VStack(alignment: .leading) {
-            HStack(spacing: 4) {
+            HStack(alignment: .bottom,spacing: 8) {
                 Text("사진")
+                    .caption14SemiBold()
+                    .foregroundStyle(.gray700)
                 Text("최대 5장")
-                    .font(.caption)
-                    .foregroundStyle(Color.gray)
+                    .caption14Regular()
+                    .foregroundStyle(.gray500)
             }
             
             ScrollView(.horizontal) {
                 HStack(spacing: 8) {
                     PhotosPicker(selection: $postInfoViewModel.selectedItems, maxSelectionCount: 5, selectionBehavior: .ordered, matching: .images) {
                         ZStack {
-                            VStack(spacing: 2) {
-                                Image(systemName: "photo.fill.on.rectangle.fill")
-                                Text("\(postInfoViewModel.selectedItems.count)/5")
-                            }
                             RoundedRectangle(cornerRadius: 10)
-                                .fill(Color.gray.opacity(0.2))
+                                .fill(.gray50)
                                 .frame(width: 84, height: 84)
+                            VStack(spacing: 2) {
+                                Image(.imageUploadIcon)
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                HStack(spacing: 0) {
+                                    if postInfoViewModel.selectedItems.count == 0 {
+                                            Text("\(postInfoViewModel.selectedItems.count)")
+                                                .caption12Regular()
+                                                .foregroundStyle(.gray500)
+                                        } else {
+                                            Text("\(postInfoViewModel.selectedItems.count)")
+                                                .caption12Bold()
+                                                .foregroundStyle(.pointSkyBlue)
+                                        }
+                                    Text("/5")
+                                        .caption12Regular()
+                                        .foregroundStyle(.gray500)
+                                }
+                            }
                         }
-                        .foregroundStyle(Color.gray)
                     }
                     
                     ForEach(postInfoViewModel.selectedImagesData.indices, id: \.self) { index in
@@ -66,10 +87,10 @@ struct PostInfoView: View {
                                 Button(action: {
                                     postInfoViewModel.removeImage(at: index)
                                 }, label: {
-                                    Image(systemName: "xmark.circle.fill")
-                                        .foregroundStyle(Color.gray)
-                                        .padding(5)
+                                    Image(.xmarkCircleFill)
                                 })
+                                .padding(.top, 4)
+                                .padding(.trailing, 5)
                             }
                         }
                     }
@@ -83,16 +104,19 @@ struct PostInfoView: View {
     private var contentView: some View {
         VStack(alignment: .leading) {
             Text("모집 내용")
+                .caption14SemiBold()
             TextField("활동 목적, 모집 인원, 활동 일정, 지원 자격 등을 자유롭게 작성해주세요", text: $postInfoViewModel.content, axis: .vertical)
                 .maxLength(text: $postInfoViewModel.content, 2000)
                 .lineLimit(2...)
                 .frame(minHeight: 168, alignment: .top)
-                .bold()
-                .padding(18)
+                .font(.pretendard(type: .medium, size: 16))
+                .padding(.vertical, config.verticalPadding)
+                .tracking(config.letterSpacing)
+                .padding(16)
                 .background(
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.clear)
-                        .strokeBorder(Color.gray.opacity(0.2), lineWidth: 1)
+                        .strokeBorder(.gray100, lineWidth: 1)
                 )
         }
     }
