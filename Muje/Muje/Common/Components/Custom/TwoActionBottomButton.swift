703942887497534690c48da80dd778cb43ab41ce
Seo <seohyun6361@gmail.com>
2025-09-11 12:56:20 +0900

    🔧[Chore]#65 - TwoActionBottomButton 간단 코드 수정
    disabled 삭제

--- Muje/Muje/Common/Components/Custom/TwoActionBottomButton.swift
+++ Muje/Muje/Common/Components/Custom/TwoActionBottomButton.swift
@@ -14,7 +14,7 @@ struct TwoActionBottomButton: View {
     var rightAction: () -> Void
     var rightText: String
     // var rightBGColor: Color = .primaryBlack
-    var nextButtonCondition: Bool = true
+    var nextButtonCondition: Bool = false
     var body: some View {
         HStack(spacing: 17) {
             Button {
@@ -39,7 +39,6 @@ struct TwoActionBottomButton: View {
                     .foregroundStyle(nextButtonCondition ? .gray400 : .graywhite)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
             }
-            .disabled(nextButtonCondition)
         }
         .padding(EdgeInsets(top: 20, leading: 16, bottom: 43, trailing: 16))
         .background(
