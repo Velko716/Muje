522a7de32978058bbda589e919e48a8ed6806806
Seo <seohyun6361@gmail.com>
2025-09-14 08:54:11 +0900

    💄[Design]#65 - 커스텀 질문 박스 디자인 반영
    폰트컬러 적용, 패딩값 수정

--- Muje/Muje/Modules/UploadPost/RecruitmentPostView/Component/CustomQuestionItemView.swift
+++ Muje/Muje/Modules/UploadPost/RecruitmentPostView/Component/CustomQuestionItemView.swift
@@ -11,17 +11,18 @@ struct CustomQuestionItemView: View {
   let question: DraftCustomQuestion
   let onTextChange: (String) -> Void
   let onDelete: () -> Void
-    
+    let config = Font.lineHeight(type: .medium, fontSize: 16, lineHeightPercent: 1.85, letterSpacingPercent: -1)
     @State private var text: String = ""
     @FocusState var isTyping: Bool
-    
     var body: some View {
         HStack {
             // FIXME: 플레이스 홀더는 랜덤 질문으로 변경
             // 랜덤 질문 따로 정리 되어 있는게 없어서 지금은 자기 소개 300자로만 넣었습니다!
             TextField("자기 소개 300자를 입력해주세요", text: $text)
                 .focused($isTyping)
-                .font(.system(size: 16))
+                .font(.pretendard(type: .medium, size: 16))
+                .padding(.vertical, config.verticalPadding)
+                .tracking(config.letterSpacing)
                 .onChange(of: text) { _, newValue in
                     onTextChange(newValue)
                 }
@@ -36,11 +37,11 @@ struct CustomQuestionItemView: View {
 
         } //: HSTACK
         .frame(maxWidth: .infinity)
-        .padding(.horizontal, 16)
-        .padding(.vertical, 21.5)
+        .padding(.horizontal, 18)
+        .padding(.vertical, 19.5)
         .background (
             RoundedRectangle(cornerRadius: 10)
-                .stroke(isTyping ? Color.blue : Color.gray, lineWidth: 1)
+                .stroke(isTyping ? .pointSkyBlue : .gray200, lineWidth: 1)
         )
     }
 }
