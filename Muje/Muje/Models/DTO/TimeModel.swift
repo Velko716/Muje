506125b4b0d171ab79c453149492ee4c0b464310
jHoon99 <eldkf1597@naver.com>
2025-09-16 00:20:25 +0900

    ✨[Feat]#85 - TimeModel DTO 구현

---
+++ Muje/Muje/Models/DTO/TimeModel.swift
@@ -0,0 +1,71 @@
+//
+//  InterviewSlot.swift
+//  Muje
+//
+//  Created by 김진혁 on 8/5/25.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct TimeModel: Equatable, Hashable, Codable {
+    let timeId: UUID
+    let postId: String
+    let startTime: Timestamp
+    let endTime: Timestamp
+    let isStartShown: Bool
+    let isEndShown: Bool
+    @ServerTimestamp var createdAt: Timestamp?
+    @ServerTimestamp var updatedAt: Timestamp?
+
+    init(
+        timeId: UUID,
+        postId: String,
+        startTime: Timestamp,
+        endTime: Timestamp,
+        isStartShown: Bool,
+        isEndShown: Bool,
+        createdAt: Timestamp?,
+        updatedAt: Timestamp?
+    ) {
+        self.timeId = timeId
+        self.postId = postId
+        self.startTime = startTime
+        self.endTime = endTime
+        self.isStartShown = isStartShown
+        self.isEndShown = isEndShown
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case timeId = "time_id"
+        case postId = "post_id"
+        case startTime = "start_time"
+        case endTime = "end_time"
+        case isStartShown = "is_start_shown"
+        case isEndShown = "is_end_shown"
+        case createdAt = "created_at"
+        case updatedAt = "updated_at"
+    }
+}
+
+
+extension TimeModel: EntityRepresentable {
+    var entityName: CollectionType { .timeModel }
+
+    var documentID: String { timeId.uuidString }
+
+    var asDictionary: [String: Any]? {
+        [
+            "time_id": timeId.uuidString,
+            "post_id": postId,
+            "start_time": startTime,
+            "end_Time": endTime,
+            "is_start_shown": isStartShown,
+            "isEndShown": isEndShown,
+            "created_at": createdAt ?? FieldValue.serverTimestamp(),
+            "updated_at": updatedAt ?? FieldValue.serverTimestamp()
+        ]
+    }
+}
