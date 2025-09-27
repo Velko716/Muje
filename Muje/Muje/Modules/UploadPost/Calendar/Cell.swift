5c9c097afc362790f9775d58716086e61287b560
Hong <dbsghdz1@gmail.com>
2025-09-28 00:48:27 +0900

    💄[Design]#89 - Cell 수정

--- Muje/Muje/Modules/UploadPost/Calendar/Cell.swift
+++ Muje/Muje/Modules/UploadPost/Calendar/Cell.swift
@@ -8,51 +8,51 @@
 import SwiftUI
 
 struct Cell: View {
-    var calendarDay: CalendarDay
-    
-    @Bindable var calendarViewModel: CalendarViewModel
-    var startDate: Date
-    var endDate: Date
-    @State var isSelectedAble: Bool = false
-    @State var isSelected: Bool = true
-    var condition: Bool?
-    
-    var body: some View {
-        ZStack {
-            if isSelectedAble || condition == true {
-                Circle()
-                    .fill(Color.yellow.opacity(0.6))
-                    .frame(width: 26, height: 27)
-                    .transition(.scale.combined(with: .opacity))
-            }
-            
-            Text("\(calendarDay.day)")
-                .font(.caption)
-                .foregroundStyle(textColor)
-                .animation(.easeInOut(duration: 0.2), value: calendarViewModel.selectedDate)
-        }
-        .frame(height: 30)
-        .task {
-            selectSlot()
-        }
-        .onChange(of: endDate) {
-            selectSlot()
-        }
+  var calendarDay: CalendarDay
+  
+  @Bindable var calendarViewModel: CalendarViewModel
+  var startDate: Date
+  var endDate: Date
+  @State var isSelectedAble: Bool = false
+  @State var isSelected: Bool = true
+  var condition: Bool?
+  
+  var body: some View {
+    ZStack {
+      if isSelectedAble || condition == true {
+        Circle()
+          .fill(Color.yellow.opacity(0.6))
+          .frame(width: 26, height: 27)
+          .transition(.scale.combined(with: .opacity))
+      }
+      
+      Text("\(calendarDay.day)")
+        .font(.caption)
+        .foregroundStyle(textColor)
+        .animation(.easeInOut(duration: 0.2), value: calendarViewModel.selectedDate)
     }
-    
-    private var textColor: Color {
-        if calendarDay.isCurrentMonth {
-            if calendarDay.isToday {
-                return Color.blue
-            } else {
-                return Color.black
-            }
-        } else {
-            return Color.gray.opacity(0.4)
-        }
+    .frame(height: 30)
+    .task {
+      selectSlot()
     }
-    
-    func selectSlot() {
-        self.isSelectedAble = (startDate < calendarDay.date && calendarDay.date < endDate)
+    .onChange(of: endDate) {
+      selectSlot()
     }
+  }
+  
+  private var textColor: Color {
+    if calendarDay.isCurrentMonth {
+      if calendarDay.isToday {
+        return Color.pointSkyBlue
+      } else {
+        return Color.black
+      }
+    } else {
+      return Color.gray.opacity(0.4)
+    }
+  }
+  
+  func selectSlot() {
+    self.isSelectedAble = (startDate < calendarDay.date && calendarDay.date < endDate)
+  }
 }
