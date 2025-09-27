2f56f4a266fec01ab175d365c79c8ec40e802b33
Hong <dbsghdz1@gmail.com>
2025-09-28 00:54:35 +0900

    💄[Design]#89 - toolTip모달 추가

--- Muje/Muje/Modules/UploadPost/Views/InterviewTimeToolTipModal.swift
+++ Muje/Muje/Modules/UploadPost/Views/InterviewTimeToolTipModal.swift
@@ -7,4 +7,45 @@
 
 import SwiftUI
 
-struct InterviewTimeToolTipModal
+struct InterviewTimeToolTipModal {
+  @Binding var closeModal: Bool
+}
+
+extension InterviewTimeToolTipModal: View {
+  var body: some View {
+    VStack {
+      Spacer()
+      Group {
+        Text("면접 시간은 설정한 전체 시간을")
+          .subheadline20SemiBold()
+        Text("나누어 배정하는 단위입니다")
+          .subheadline20SemiBold()
+      }
+      .foregroundStyle(Color.grayblack)
+      Spacer()
+      if #available(iOS 26, *) {
+        Image(.interviewToolTipImage26)
+          .resizable()
+          .frame(width: 361, height: 94)
+      } else {
+        Image(.interviewTimeToolTip)
+          .resizable()
+          .frame(width: 361, height: 94)
+      }
+      Spacer()
+      Button {
+        closeModal = false
+      } label: {
+        Text("확인")
+          .body1SemiBold18()
+          .foregroundStyle(.graywhite)
+          .frame(maxWidth: .infinity)
+          .padding()
+      }
+      .background(Color.primaryBlack)
+      .clipShape(RoundedRectangle(cornerRadius: 10))
+      .padding()
+      Spacer()
+    }
+  }
+}
